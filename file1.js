ba6590f3908b51e87ae17ba7379bdaac7e2e546b
GraceAyoade <tomilewagrace1@gmail.com>
2024-08-20 19:57:37 +0100

    Fixed bug in file1.js

--- file1.js
+++ file1.js
@@ -2,9 +2,10 @@
 
 function calculateSum(numbers) {
     return numbers.reduce((acc, num) => {
-        acc += num;
+       return acc += num;
     }, 0);
 }
 
 const nums = [1, 2, 3, 4, 5];
 console.log("Sum:", calculateSum(nums)); // Expected output: 15, but will log: undefined
+//the return keyword was not used to return the final sum.
