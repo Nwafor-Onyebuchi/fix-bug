3c29ad2d66379cbb48ac7eea49f87ecfb1085cf4
Ogunleye Gbenga <oguns45@gmail.com>
2024-08-20 18:35:41 +0100

    Fixed bug in file2.js

--- file2.js
+++ file2.js
@@ -1,7 +1,7 @@
 // file2.js
 
 function isEven(number) {
-    if (number % 2 = 0) { 
+    if (number % 2 === 0) { ///strict equality operator.
         return true;
     } else {
         return false;
@@ -10,3 +10,7 @@ function isEven(number) {
 
 console.log("4 is even:", isEven(4)); // Expected output: true, but will log: false
 console.log("7 is even:", isEven(7)); // Expected output: false, but will log: false
+
+
+////4 is even: true
+///7 is even: false
