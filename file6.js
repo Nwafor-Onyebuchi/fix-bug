55ad1651b45808a7ba0f87a94f833796eafb0fa7
PreciousAnyi <jkpresh@gmail.com>
2024-08-20 16:34:37 +0100

    Fixed bug in file6.js

--- file6.js
+++ file6.js
@@ -1,7 +1,8 @@
 // file6.js
 
 function filterEvenNumbers(numbers) {
-    return numbers.filter(num => num % 2);  
+    //I fixed this by doing num % 2 == 0
+    return numbers.filter(num => num % 2 == 0);  
 }
 
 console.log("Even numbers:", filterEvenNumbers([1, 2, 3, 4, 5])); // Expected output: [2, 4]
